dae899b15c6442df50265dcba2f0c1d794a376fe
wwysopal <wioletta.wysopal@gmail.com>
2025-12-05 12:49:02 +0100

    Install React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
-import InsertionSortVisualizer from './components/InsertionSortVisualizer'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import InsertionSortVisualizer from "./components/InsertionSortVisualizer";
 
 function App() {
   return (
-    <div>
+    <Router>
       <nav className="navbar navbar-dark bg-dark w-100">
-        <h1 className="text-center w-100 text-white m-0 display-5"> AlgorithmLab </h1>
+        <h1 className="text-center w-100 text-white m-0 py-3 display-5">
+          AlgorithmLab
+        </h1>
       </nav>
 
-      <InsertionSortVisualizer />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/insertion-sort" element={<InsertionSortVisualizer />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="container mt-5">
+      <h2 className="text-center mb-4">Choose an Algorithm</h2>
+
+      <div className="row justify-content-center g-4">
+
+        <div className="col-md-3">
+          <Link to="/insertion-sort" className="text-decoration-none">
+            <div className="card shadow text-center p-4 bg-primary text-white">
+              <h4>Insertion Sort</h4>
+            </div>
+          </Link>
+        </div>
+
+        {/* Możesz dodać kolejne kafelki */}
+        <div className="col-md-3">
+          <div className="card shadow text-center p-4 bg-secondary text-white">
+            <h4>Soon...</h4>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Home;
